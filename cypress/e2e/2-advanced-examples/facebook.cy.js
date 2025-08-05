3346a11ebd354dec3c2c4b411b007ebb70c75f70
Pilar Forero <pilarandrea@gmail.com>
2025-08-05 17:45:10 -0500

    Section 8

---
+++ cypress/e2e/2-advanced-examples/facebook.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+context("Actions", () => {
+  // beforeEach(() => {
+  //   cy.visit('https://example.cypress.io/commands/actions')
+  // })
+
+  // https://on.cypress.io/interacting-with-elements
+
+  it(".type() - type into a DOM element", () => {
+    cy.visit("https://facebook.com"); 
+  });
+});
